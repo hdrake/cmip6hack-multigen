7ef1ac0db083163f54df54bbd05bfb009fdef905
Henri Drake <henrifdrake@gmail.com>
2020-03-06 03:13:43 +0000

    Starting re-factoring code into functions
    Trying to re-factor code with the temporary goal of reading in all data,
    computing time-mean, and saving locally in just a few lines.

--- notebooks/util.py
+++ notebooks/util.py
@@ -5,6 +5,10 @@
 
 import re
 import socket
+import numpy as np
+import xarray as xr
+import xesmf as xe
+import os, sys
 
 def is_ncar_host():
     """Determine if host is an NCAR machine."""
@@ -13,3 +17,37 @@ def is_ncar_host():
     return any([re.compile(ncar_host).search(hostname) 
                 for ncar_host in ['cheyenne', 'casper', 'hobart']])
 
+
+
+
+# Define the common target grid axes
+dlon, dlat = 1., 1.
+ds_out = xr.Dataset({'lat': (['lat'], np.arange(-90.+dlat/2., 90., dlat)),
+                     'lon': (['lon'], np.arange(0.+dlon/2., 360., dlon)),})
+
+# Regridding function
+def regrid_to_common(ds, ds_out=ds_out):
+    """
+    Regrid from rectilinear grid to common grid
+    """
+    regridder = xe.Regridder(ds, ds_out, 'bilinear',periodic=True, reuse_weights=True)
+    return regridder(ds)
+
+def calc_area(lat, lon, coarsen_size=1., dlat=1., dlon=1.):
+    Rearth = 6.378E6   # radius of Earth in meters
+    if coarsen != 1.:
+        return (
+        (np.deg2rad(dlat)*Rearth) * (np.deg2rad(dlon)*Rearth*np.cos(np.deg2rad(lat))) * xr.ones_like(lon)
+        ).coarsen({'lat': coarsen_size, 'lon': coarsen_size}, boundary='exact').mean()
+    else:
+        return (np.deg2rad(dlat)*Rearth) * (np.deg2rad(dlon)*Rearth*np.cos(np.deg2rad(lat))) * xr.ones_like(lon)
+
+
+class HiddenPrints:
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
