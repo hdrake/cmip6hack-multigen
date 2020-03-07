9f9c4e272738067609e856ad94a2c5682fd0c2c2
Henri Drake <henrifdrake@gmail.com>
2020-03-07 16:26:18 +0000

    Initial commit of re-factored preprocessing funcs
    I split the preprocessing functions into three submodules:
    preprocessing, utility functions (e.g. calculating the area or ensemble
    mean), and quality control (e.g. correcting errors in some of the models
    or converting to CF-compliant SI units).
    
    A dictionary of xarray.Dataset objects (each representing one
    acitivity_id) is lazily loaded using the `preprocess.load_ensembles`
    function. xarray methods can be passed to objects within the dictionary
    using the `dict_func` method, which will eventually be replaced by a
    more convenient xarray-like API.

--- notebooks/util.py
+++ notebooks/util.py
@@ -8,7 +8,9 @@ import socket
 import numpy as np
 import xarray as xr
 import xesmf as xe
+import pandas as pd
 import os, sys
+from tqdm.autonotebook import tqdm  # Fancy progress bars for our loops!
 
 def is_ncar_host():
     """Determine if host is an NCAR machine."""
@@ -42,7 +44,6 @@ def calc_area(lat, lon, coarsen_size=1., dlat=1., dlon=1.):
     else:
         return (np.deg2rad(dlat)*Rearth) * (np.deg2rad(dlon)*Rearth*np.cos(np.deg2rad(lat))) * xr.ones_like(lon)
 
-
 class HiddenPrints:
     def __enter__(self):
         self._original_stdout = sys.stdout
@@ -51,3 +52,27 @@ class HiddenPrints:
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.close()
         sys.stdout = self._original_stdout
+        
+def vec_dt_replace(series, year=None, month=None, day=None):
+    return pd.to_datetime(
+        {'year': series.dt.year if year is None else year,
+         'month': series.dt.month if month is None else month,
+         'day': series.dt.day if day is None else day})
+
+def add_ens_mean(ens_dict):
+    for mip_id, ens in ens_dict.items():
+        ensmean = ens.mean(dim='ensemble', skipna=True)
+        ens = xr.concat([ensmean.expand_dims({'ensemble': np.array(['ens-mean'])}), ens], dim='ensemble')
+        ens.attrs['name'] = mip_id
+        ens_dict[mip_id] = ens
+    return ens_dict
+
+def dict_func(d, func, on_self=False, *args, **kwargs):
+    new_d = {}
+    for key, item in tqdm(d.items()):
+        if on_self:
+            new_d[key] = func(self=d[key], *args, **kwargs)
+        else:
+            new_d[key] = func(d[key], *args, **kwargs)
+        
+    return new_d
